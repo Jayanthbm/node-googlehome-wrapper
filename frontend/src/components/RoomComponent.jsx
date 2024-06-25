980abcf072613632f941b8fb7a266b226df16722
Jayanth <jayanthbharadwajm@gmail.com>
2024-06-25 19:30:00 +0530

    keys for very item added

--- frontend/src/components/RoomComponent.jsx
+++ frontend/src/components/RoomComponent.jsx
@@ -1,13 +1,13 @@
 import { HomeOutlined } from "@ant-design/icons";
 import { Breadcrumb } from "antd";
 import React from "react";
+import CardGrid from "./CardGrid";
 import FanComponent from "./FanComponent";
 import LightComponent from "./LightComponent";
 import OutletComponent from "./OutletComponent";
 import SpeakerComponent from "./SpeakerComponent";
-import CardGrid from "./CardGrid";
 
-const RoomComponent = ({ name, data, forceReload }) => {
+const RoomComponent = ({ name, data }) => {
   return (
     <>
       <Breadcrumb
@@ -26,20 +26,16 @@ const RoomComponent = ({ name, data, forceReload }) => {
         {data.map((device, index) => {
           switch (device.deviceType) {
             case "Fan": {
-              return <FanComponent data={device} forceReload={forceReload} />;
+              return <FanComponent data={device} key={index} />;
             }
             case "Outlet": {
-              return (
-                <OutletComponent data={device} forceReload={forceReload} />
-              );
+              return <OutletComponent data={device} key={index} />;
             }
             case "Light": {
-              return <LightComponent data={device} forceReload={forceReload} />;
+              return <LightComponent data={device} key={index} />;
             }
             case "Speaker": {
-              return (
-                <SpeakerComponent data={device} forceReload={forceReload} />
-              );
+              return <SpeakerComponent data={device} key={index} />;
             }
             default: {
               return <></>;
